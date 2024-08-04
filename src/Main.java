84c863c07ca5f119940c4a7a69977dbaa95fc12e
Elmirbek Surkeev <elmirsurkeev@gmail.com>
2024-08-05 04:15:35 +0600

    added full updates

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Capacity bottle = new Capacity("bootle", "plasmassa", 12);
-        Mug mug = new Mug("mug", "farfor", 0.4, true) {
-            @Override
-            public void displayMug() {
-                super.displayMug();
-            }
-        };
+        Capacity packetMilk = new Capacity("Granmother home ", " polietilen", 1);
+        CapacityWithLid bottle = new CapacityWithLid("bootle", "plasmassa", 1.5, true);
+        CapacityWithLid pot = new CapacityWithLid("pot", "iron", 12, true);
+        CapacityWithLid container = new CapacityWithLid("container", "plasmassa", 4, false);
+        CapacityWithLid containerWithLid = new CapacityWithLid("container", "plasmassa", 4.5, true);
+
+        Mug mug = new Mug("mug", "farfor", 0.4, true){};
+        packetMilk.display();
+        bottle.displayLid();
+        pot.displayLid();
+        container.displayLid();
+        containerWithLid.displayLid();
         mug.displayMug();
     }
 }
